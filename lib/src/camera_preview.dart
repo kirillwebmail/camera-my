cca8697eddee3862e63b2c76d8b77396a39c3fb4
kirillkaluznyj <kirill.webmail@yandex.com>
2021-09-09 17:46:05 +0300

    aspect ratio

--- lib/src/camera_preview.dart
+++ lib/src/camera_preview.dart
@@ -25,9 +25,8 @@ class CameraPreview extends StatelessWidget {
             valueListenable: controller,
             builder: (context, value, child) {
               return AspectRatio(
-                aspectRatio: _isLandscape()
-                    ? controller.value.aspectRatio
-                    : (1 / controller.value.aspectRatio),
+                aspectRatio:
+                    _isLandscape() ? controller.value.aspectRatio : (4 / 3),
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
