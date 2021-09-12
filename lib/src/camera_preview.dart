ef5604f8489d85ae690ef95a00f4e627596cd443
kirillkaluznyj <kirill.webmail@yandex.com>
2021-09-12 12:18:07 +0300

    fit: StackFit.expand

--- lib/src/camera_preview.dart
+++ lib/src/camera_preview.dart
@@ -25,6 +25,7 @@ class CameraPreview extends StatelessWidget {
             valueListenable: controller,
             builder: (context, value, child) {
               return Stack(
+                fit: fit: StackFit.expand,
                   children: [
                     _wrapInRotatedBox(child: controller.buildPreview()),
                     child ?? Container(),
