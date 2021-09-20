198095cbc4d0e3286ea21210b1268a4b4a2f68c4
kirillkaluznyj <kirill.webmail@yandex.com>
2021-09-20 09:15:24 +0300

    22222

--- android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -240,7 +240,7 @@ class Camera implements CameraCaptureCallback.CameraCaptureStateListener, ImageR
           Log.w(TAG, "********" + resolutionFeature.getPreviewSize().getWidth() + "*********"
               + resolutionFeature.getPreviewSize().getHeight());
 
-          dartMessenger.sendCameraInitializedEvent(1620, 2160, cameraFeatures.getExposureLock().getValue(),
+          dartMessenger.sendCameraInitializedEvent(2160, 1620, cameraFeatures.getExposureLock().getValue(),
               cameraFeatures.getAutoFocus().getValue(), cameraFeatures.getExposurePoint().checkIsSupported(),
               cameraFeatures.getFocusPoint().checkIsSupported());
         } catch (CameraAccessException e) {
