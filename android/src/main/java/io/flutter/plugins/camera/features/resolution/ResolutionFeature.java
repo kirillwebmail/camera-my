95a2fc9f718e2e7136a6710c5c0eadfa6f95483b
kirillkaluznyj <kirill.webmail@yandex.com>
2021-11-10 22:04:30 +0300

    ййц

--- android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
+++ android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
@@ -156,7 +156,7 @@ public class ResolutionFeature extends CameraFeature<ResolutionPreset> {
     Log.w(TAG, "000000   " + newCaptureSize.getHeight());
 
     if (isSquare) {
-      return new Size(newCaptureSize.getHeight() * 0.5, newCaptureSize.getHeight());
+      return new Size(newCaptureSize.getHeight() + 200, newCaptureSize.getHeight());
     }
 
     if (newCaptureSize.getHeight() / newCaptureSize.getWidth() != 0.75) {
